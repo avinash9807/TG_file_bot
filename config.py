31686e360066bfc15a3958cf9737151b00d20a41
Avinash9807 <82309253+avinash9807@users.noreply.github.com>
2025-12-27 14:35:34 +0530

    Add LOG_CHANNEL_ID for file storage
    Added new environment variable for log channel ID.

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
 # --- SECURE CONFIG ---
-# Ab hum values direct yahan nahi likhenge
-# Ye values hum Koyeb ki Settings me dalenge
 
 API_ID = int(os.environ.get("API_ID", "0"))
 
@@ -10,8 +8,13 @@ API_HASH = os.environ.get("API_HASH", "")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-# Owner IDs ko comma se split karke list banayenge
+# Owner IDs (Jinke paas access hai)
 OWNER_IDS = list(map(int, os.environ.get("OWNER_IDS", "0").split(",")))
 
+# Koyeb App URL
 APP_URL = os.environ.get("APP_URL", "")
+
+# NEW: Storage Channel ID (Jahan files save hongi)
+# Default 0 hai, hum ise Koyeb Environment Variables se lenge
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", "0"))
 
