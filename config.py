f54ede7327e0c01f3b851f2f7d9fa653d04ba6b9
Avinash9807 <82309253+avinash9807@users.noreply.github.com>
2025-12-27 14:08:07 +0530

    Initialize config.py with Telegram bot settings
    Added configuration settings for Telegram bot.

---
+++ config.py
@@ -0,0 +1,18 @@
+# --- CONFIGURATION FILE ---
+
+# 1. my.telegram.org se mila ID (Number)
+API_ID = 32531340
+
+# 2. my.telegram.org se mila Hash (Quotes me)
+API_HASH = "b0f3d86403c3a39f5f1bf3075376e610"
+
+# 3. BotFather wala Token
+BOT_TOKEN = "8370777456:AAH_V4uGlWxJreUaozi_lNSh_DRJzZvSaI4"
+
+# 4. Yahan dono maalik ki ID dalein (Comma laga kar)
+# Example: [123456789, 987654321]
+OWNER_IDS = [8204103018, 800000580]
+
+# 5. Koyeb App URL (Isse baad me update karenge)
+# Example: "https://tg-bot.koyeb.app" (Bina last slash ke)
+APP_URL = "https://example.com"
